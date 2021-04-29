d78989054dc0da8e298bda1d5938ef97b2218afb
Atsushi <atsushithesushi@gmail.com>
2021-04-29 10:04:09 -0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import '../sassGlobal/globals.scss';
-import '../cssGlobal/normalize.css'
+import '../cssGlobal/normalize.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
